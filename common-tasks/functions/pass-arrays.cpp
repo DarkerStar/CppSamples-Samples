3c2b43eac0b96e04d5c804a781a7a741e42f1597
Mark A. Gibbs <indi.in.the.wired@gmail.com>
2018-04-10 17:37:12 -0400

    Rewrote passing arrays example to remove references to dynarray

--- common-tasks/functions/pass-arrays.cpp
+++ common-tasks/functions/pass-arrays.cpp
@@ -1,48 +1,62 @@
 // Pass arrays
-// C++11, Experimental
+// C++11
 
 #include <array>
-#include <experimental/dynarray>
+#include <vector>
 
-void compile_time(std::array<int, 3> arr)
-{ }
+void fixed_size(std::array<int, 3> array);
 
-void run_time(std::experimental::dynarray<int> arr)
-{ }
+void dynamic_size(std::vector<int> array);
 
 int main()
 {
-  std::array<int, 3> arr = {4, 8, 15};
-  compile_time(arr);
-  compile_time({16, 23, 42});
-
-  std::experimental::dynarray<int> dynarr = {1, 2, 3};
-  run_time(dynarr);
-  run_time({1, 2, 3, 4, 5});
+  fixed_size({1, 2, 3});
+  
+  std::array<int, 3> fixed_sized_array;
+  // ... fill fixed_sized_array with values somehow
+  fixed_size(fixed_sized_array);
+  
+  dynamic_size({1, 2, 3, 4, 5});
+  
+  std::vector<int> dynamic_sized_array;
+  // ... fill dynamic_sized_array with values somehow
+  dynamic_size(dynamic_sized_array);
 }
 
-// Pass fixed-size arrays to and from functions.
+// Pass arrays of values to functions.
 //
-// Built-in array types are not copyable, so cannot be passed to and
-// from functions by value. The traditional approach is to pass both a
+// Built-in array types are not copyable, and so cannot be passed to
+// functions by value. The traditional approach is to pass both a
 // pointer to an array's first element and the size of the array.
 // However, C++ provides class types that represent copyable
-// fixed-size arrays.
+// arrays.
 //
-// We define the function `compile_time`, on [7-8], to have a
-// parameter of type [`std::array`](cpp/container/array), which represents
-// *compile-time* fixed-size arrays. This means that the size must be
-// known at compile time. A template may be used to instantiate
-// functions for different `std::array` sizes. [!15-17] demonstrate
-// passing arrays of size 3 to this function.
+// We can create functions that take a compile-time fixed-size array
+// of values by using [`std::array`](cpp/container/array) as the
+// parameter type, as on [7]. We can call such functions using an
+// initializer list of values directly as on [13], or by copying
+// values into an array that we can pass as an argument as on [15-17].
 //
-// On [10-11], we define function `run_time` to have a parameter of type
-// [`std::experimental::dynarray`](cpp/container/dynarray), which
-// represents *run-time* fixed-size arrays. This type can be created with
-// any size at run time, but cannot change size once created. [!19-21]
-// demonstrate passing arrays of various sizes to this function.
+// With fixed-sized arrays, the compiler enforces that the correct
+// number of values is passed. (So if less than 3 values are used on
+// [13], the remainder will be value-initialized, which for `int`
+// means they will be set to 0.)
 //
-// **Note**: `std::experimental::dynarray` is part of the Arrays
-// Technical Specification, which provides experimental features that
-// may soon be introduced to the C++ standard. It should not be used
-// in production code.
+// If the number of values is not known at compile time, a
+// [`std::vector`](cpp/container/vector) must be used. A `vector` is
+// a C++ run-time-sized or dynamically-sized array. [!9] illustrates a
+// function accepting an array of any number of values. It can be
+// called using an initializer list of any size as on [19], or by
+// copying values into a `vector` that we can pass as an argument as
+// on [21-23].
+//
+// Note that passing arrays of values is expensive, as it requires
+// copying all the elements of the array one-by-one. Also, `vector`s
+// require run-time memory allocations. For that reason it is usually
+// preferable to pass array *views* rather than arrays.
+
+void fixed_size(std::array<int, 3>)
+{ }
+
+void dynamic_size(std::vector<int>)
+{ }
